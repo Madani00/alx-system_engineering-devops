80864339c1f669c1958ab10ae913dd0a7c915887
Madani00 <mamchart@gmail.com>
2024-04-19 09:42:57 -0700

    well done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-
+# Using Puppet, create a file in /tmp .
 
 file { '/tmp/school':
-  ensure  => 'present',
+  ensure  => 'file',
   content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
