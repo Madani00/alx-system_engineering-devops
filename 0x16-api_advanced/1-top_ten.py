0424cb78b53d143e3ca187971c3e7e68be551e2d
Madani00 <mamchart@gmail.com>
2024-06-05 00:57:50 +0100

    fix pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""my module"""
+"""
+my module
+"""
 import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """prints the titles of the first 10 hot posts"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
