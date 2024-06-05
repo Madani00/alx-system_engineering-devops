4bbe4b6a05d49f2c28b370e66188f5a456f67844
Madani00 <mamchart@gmail.com>
2024-06-05 09:56:46 -0700

    fix pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""
-my module
-"""
-import requests
+"""Implement number_of_subscribers function"""
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    """Queries the Reddit API and returns the number of subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'custom user-agent'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
-        return 0
+        results = response.json().get('data')
+        return results.get('subscribers')
+    return 0
