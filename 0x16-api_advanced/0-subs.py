db9b030ce1b7ead1be9ed1411058b50f527a1cd1
Madani00 <mamchart@gmail.com>
2024-06-05 17:02:10 -0700

    good

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Implement number_of_subscribers function"""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'custom user-agent'}
+    headers = {'User-Agent': 'MadaniUser/1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
