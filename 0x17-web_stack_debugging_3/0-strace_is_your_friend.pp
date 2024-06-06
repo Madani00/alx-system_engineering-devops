bc5a216c89f5107ee9d574395cca2a9f42c0afdc
Madani00 <mamchart@gmail.com>
2024-06-06 17:45:03 +0000

    well done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# find out why Apache is returning a 500 error and fix the 500 error
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
